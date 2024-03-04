884262b4fcd55d1cf31fbeba967b223b823ea57b
Sourav Patel <37928615+aroundarmor@users.noreply.github.com>
2024-03-04 16:33:13 +0530

    final main.tf

--- main.tf
+++ main.tf
@@ -14,19 +14,19 @@ resource "aws_sns_topic" "example" {
 
 # Create SNS topic policy allowing publish and subscribe for all principals
 resource "aws_sns_topic_policy" "example" {
-  arn    = aws_sns_topic.example.arn
+  arn = aws_sns_topic.example.arn
   policy = jsonencode({
     Version = "2012-10-17"
     Id      = "ExampleTopicPolicy"
     Statement = [{
       Effect    = "Allow"
       Principal = "*"
-      Action    = [
+      Action = [
         "SNS:Subscribe",
         "SNS:Publish",
         "SNS:Receive",
       ]
-      Resource  = aws_sns_topic.example.arn
+      Resource = aws_sns_topic.example.arn
     }]
   })
 }
@@ -55,30 +55,30 @@ locals {
 # Reconstructing the cron expression with the adjusted hour value
 locals {
   adjusted_cron_expression = join(" ", concat(
-    slice(local.cron_parts, 0, 1),  // Minute
-    [local.adjusted_hour],           // Adjusted hour
-    slice(local.cron_parts, 2, 6)    // Remaining cron parts
+    slice(local.cron_parts, 0, 1), // Minute
+    [local.adjusted_hour],         // Adjusted hour
+    slice(local.cron_parts, 2, 6)  // Remaining cron parts
   ))
 }
 
-output "adjusted_cron_expression" {
-  value = local.adjusted_cron_expression
-}
+# output "adjusted_cron_expression" {
+#   value = local.adjusted_cron_expression
+# }
 
 ####################################################################
 ####################################################################
 # Create maintenance windows
 resource "aws_ssm_maintenance_window" "example" {
-  name                     = var.mw_name
-  schedule                 = var.schedule
-  cutoff                   = var.cutoff
-  duration                 = var.duration
+  name                       = var.mw_name
+  schedule                   = var.schedule
+  cutoff                     = var.cutoff
+  duration                   = var.duration
   allow_unassociated_targets = var.allow_unassociated_targets
-  enabled                  = var.enabled
-  end_date                 = var.end_date
-  schedule_timezone        = var.schedule_timezone
-  schedule_offset          = var.schedule_offset
-  start_date               = var.start_date
+  enabled                    = var.enabled
+  end_date                   = var.end_date
+  schedule_timezone          = var.schedule_timezone
+  schedule_offset            = var.schedule_offset
+  start_date                 = var.start_date
 }
 
 ####################################################################
@@ -88,8 +88,8 @@ resource "aws_ssm_maintenance_window_target" "example" {
   window_id     = aws_ssm_maintenance_window.example.id
   resource_type = var.resource_type
   targets {
-    key = "tag:${var.patch_group_tag}"
-    values = var.patch_group_tag_value
+    key    = "tag:${var.patch_group_tag}"
+    values = [var.patch_group_tag_value]
   }
 }
 
@@ -97,40 +97,40 @@ resource "aws_ssm_maintenance_window_target" "example" {
 ####################################################################
 # Create maintenance window tasks
 resource "aws_ssm_maintenance_window_task" "example_task" {
-  window_id          = aws_ssm_maintenance_window.example.id
-  max_concurrency    = var.max_concurrency
-  max_errors         = var.max_errors
-  cutoff_behavior    = var.cutoff_behavior
-  task_type          = var.task_type
-  task_arn           = var.task_arn
-  service_role_arn   = "arn:aws:iam::${var.account_id}:role/OllionPatchingAutomation"
-  priority           = var.priority
+  window_id        = aws_ssm_maintenance_window.example.id
+  max_concurrency  = var.max_concurrency
+  max_errors       = var.max_errors
+  cutoff_behavior  = var.cutoff_behavior
+  task_type        = var.task_type
+  task_arn         = var.task_arn
+  service_role_arn = "arn:aws:iam::${var.account_id}:role/OllionPatchingAutomation"
+  priority         = var.priority
   targets {
-    key = "WindowTargetIds"
+    key    = "WindowTargetIds"
     values = aws_ssm_maintenance_window_target.example[*].id
   }
 
   task_invocation_parameters {
     run_command_parameters {
       parameter {
-        name = "Operation"
+        name   = "Operation"
         values = var.operation
       }
       parameter {
-        name = "RebootOption"
+        name   = "RebootOption"
         values = var.reboot_option
       }
 
-      output_s3_bucket = aws_s3_bucket.maintenance_window_bucket.id
+      output_s3_bucket     = aws_s3_bucket.maintenance_window_bucket.id
       output_s3_key_prefix = "/output_logs/${var.mw_name}"
 
-      notification_config {
-        notification_arn = aws_sns_topic.example.arn
-        notification_events = ["Success"]
-        notification_type = "Command"
+      # notification_config {
+      #   notification_arn    = aws_sns_topic.example.arn
+      #   notification_events = ["Success"]
+      #   notification_type   = "Command"
 
-      }
-      
+      # }
+
       cloudwatch_config {
         cloudwatch_log_group_name = "aws/ssm/AWS-RunPatchBaseline-${var.mw_name}"
         cloudwatch_output_enabled = var.cloudwatch_output_enabled
@@ -143,51 +143,101 @@ resource "aws_ssm_maintenance_window_task" "example_task" {
 ####################################################################
 # Create patch baseline
 resource "aws_ssm_patch_baseline" "example" {
-  name                              = "Ollion-Patching-${var.operating_system}"
-  description                       = "This is a patch baseline for tag:${var.patch_group_tag} in ${var.operating_system}"
-  operating_system                  = var.operating_system
-  approved_patches                  = var.approved_patches
-  rejected_patches_action           = var.rejected_patches_action
-  rejected_patches                  = var.rejected_patches
+  name                    = "Ollion-Patching-${var.operating_system}"
+  description             = "This is a patch baseline for tag:${var.patch_group_tag} in ${var.operating_system}"
+  operating_system        = var.operating_system
+  approved_patches        = var.approved_patches
+  rejected_patches_action = var.rejected_patches_action
+  rejected_patches        = var.rejected_patches
 
-  approval_rule {
-    approve_after_days = var.approve_after_days
-    approve_until_date = var.approve_until_date
-    compliance_level = var.compliance_level
-    enable_non_security = var.enable_non_security
-    patch_filter {
-      key = "PRODUCT"
-      values = var.patch_filter_product
+  dynamic "approval_rule" {
+    for_each = var.operating_system == "WINDOWS" ? [1] : []
+
+    content {
+      approve_after_days  = var.approve_after_days
+      approve_until_date  = var.approve_until_date
+      compliance_level    = var.compliance_level
+      enable_non_security = var.enable_non_security
+
+      dynamic "patch_filter" {
+        for_each = var.windows_patch_filter
+
+        content {
+          key    = patch_filter.key
+          values = patch_filter.value
+        }
+      }
     }
-    patch_filter {
-      key = "CLASSIFICATION"
-      values = var.patch_filter_classification
+  }
+
+  dynamic "approval_rule" {
+    for_each = var.operating_system == "DEBIAN" || var.operating_system == "UBUNTU" ? [1] : []
+
+    content {
+      approve_after_days  = var.approve_after_days
+      approve_until_date  = var.approve_until_date
+      compliance_level    = var.compliance_level
+      enable_non_security = var.enable_non_security
+
+      dynamic "patch_filter" {
+        for_each = var.debian_patch_filter
+
+        content {
+          key    = patch_filter.key
+          values = patch_filter.value
+        }
+      }
     }
-        patch_filter {
-      key = "PRIORITY"
-      values = var.patch_filter_priority
+  }
+
+  dynamic "approval_rule" {
+    for_each = var.operating_system == "MACOS" ? [1] : []
+
+    content {
+      approve_after_days  = var.approve_after_days
+      approve_until_date  = var.approve_until_date
+      compliance_level    = var.compliance_level
+      enable_non_security = var.enable_non_security
+
+      dynamic "patch_filter" {
+        for_each = var.macos_patch_filter
+
+        content {
+          key    = patch_filter.key
+          values = patch_filter.value
+        }
+      }
     }
-        patch_filter {
-      key = "SEVERITY"
-      values = var.patch_filter_severity
-    }
-        patch_filter {
-      key = "PRODUCT_FAMILY"
-      values = var.patch_filter_product_family
-    }
-        patch_filter {
-      key = "MSRC_SEVERITY"
-      values = var.patch_filter_msrc_severity
+  }
+
+  dynamic "approval_rule" {
+    for_each = var.operating_system != "WINDOWS" && var.operating_system != "DEBIAN" && var.operating_system != "UBUNTU" && var.operating_system != "MACOS" ? [1] : []
+
+    content {
+      approve_after_days  = var.approve_after_days
+      approve_until_date  = var.approve_until_date
+      compliance_level    = var.compliance_level
+      enable_non_security = var.enable_non_security
+
+      dynamic "patch_filter" {
+        for_each = var.default_patch_filter
+
+        content {
+          key    = patch_filter.key
+          values = patch_filter.value
+        }
+      }
     }
   }
 }
+
 
 ####################################################################
 ####################################################################
 # Create patch group
 resource "aws_ssm_patch_group" "example" {
   baseline_id = aws_ssm_patch_baseline.example.id
-  patch_group = var.patch_group_tag_value  # Using maintenance window name as patch group name, adjust if needed
+  patch_group = var.patch_group_tag_value # Using maintenance window name as patch group name, adjust if needed
 }
 
 ####################################################################
@@ -281,17 +331,17 @@ resource "aws_iam_role" "scheduler_role" {
   name = "scheduler_execution_role"
 
   assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
+    "Version" : "2012-10-17",
+    "Statement" : [
       {
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "scheduler.amazonaws.com"
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "scheduler.amazonaws.com"
         },
-        "Action": "sts:AssumeRole",
-        "Condition": {
-          "StringEquals": {
-            "aws:SourceAccount": [var.account_id]
+        "Action" : "sts:AssumeRole",
+        "Condition" : {
+          "StringEquals" : {
+            "aws:SourceAccount" : [var.account_id]
           }
         }
       }
@@ -304,13 +354,13 @@ resource "aws_iam_policy" "scheduler_policy" {
   description = "Policy for AWS Scheduler execution"
 
   policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
+    "Version" : "2012-10-17",
+    "Statement" : [
       {
-        "Effect": "Allow",
-        "Action": "lambda:InvokeFunction",
-        "Resource": [
-          "aws_lambda_function.lambda_start_function.arn/*",
+        "Effect" : "Allow",
+        "Action" : "lambda:InvokeFunction",
+        "Resource" : [
+          # "aws_lambda_function.lambda_start_function.arn/*",
           aws_lambda_function.lambda_start_function.arn
         ]
       }
@@ -327,27 +377,31 @@ resource "aws_iam_role_policy_attachment" "scheduler_policy_attachment" {
 
 
 resource "aws_scheduler_schedule" "start" {
-  state = var.start_instance
-  name = "ollion-patch-instance-start-${var.mw_name}"
+  state      = var.start_instance
+  name       = "ollion-patch-instance-start-${var.mw_name}"
   group_name = "default"
 
   flexible_time_window {
-    mode = "FLEXIBLE"
+    mode                      = "FLEXIBLE"
     maximum_window_in_minutes = 30
   }
 
-  schedule_expression = "${terraform.output.adjusted_cron_expression}"
+  schedule_expression          = local.adjusted_cron_expression
   schedule_expression_timezone = var.schedule_timezone
 
+
   target {
-    arn = "arn:aws:scheduler:::aws-sdk:lambda:invoke"
+    arn      = "arn:aws:scheduler:::aws-sdk:lambda:invoke"
     role_arn = aws_iam_role.scheduler_role.arn
-    input = {
-      "patchgroup_values": var.patch_group_tag_value
-    }
+    input = jsonencode({
+      "FunctionName" : aws_lambda_function.lambda_start_function.function_name,
+      "Payload" : jsonencode({ "patchgroup_values" : var.patch_group_tag_value }),
+      "InvocationType" : "Event"
+    })
   }
-  depends_on = [ aws_lambda_function.lambda_start_function, aws_iam_role.scheduler_role ]
+  depends_on = [aws_lambda_function.lambda_start_function, aws_iam_role.scheduler_role]
 }
+
 
 ####################################################################
 ####################################################################
