52312539f536e4b4870cafe93a9e4d392b2856a4
Sourav Patel <37928615+aroundarmor@users.noreply.github.com>
2024-03-04 13:56:18 +0530

    Latest main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+
 # Create S3 bucket
 resource "aws_s3_bucket" "maintenance_window_bucket" {
   bucket = "ollion-maintenance-window-${var.account_id}-in-${var.region}"
@@ -60,6 +61,10 @@ locals {
   ))
 }
 
+output "adjusted_cron_expression" {
+  value = local.adjusted_cron_expression
+}
+
 ####################################################################
 ####################################################################
 # Create maintenance windows
@@ -68,7 +73,6 @@ resource "aws_ssm_maintenance_window" "example" {
   schedule                 = var.schedule
   cutoff                   = var.cutoff
   duration                 = var.duration
-  description              = var.description
   allow_unassociated_targets = var.allow_unassociated_targets
   enabled                  = var.enabled
   end_date                 = var.end_date
@@ -99,7 +103,7 @@ resource "aws_ssm_maintenance_window_task" "example_task" {
   cutoff_behavior    = var.cutoff_behavior
   task_type          = var.task_type
   task_arn           = var.task_arn
-  service_role_arn   = var.service_role_arn
+  service_role_arn   = "arn:aws:iam::${var.account_id}:role/OllionPatchingAutomation"
   priority           = var.priority
   targets {
     key = "WindowTargetIds"
@@ -122,7 +126,7 @@ resource "aws_ssm_maintenance_window_task" "example_task" {
 
       notification_config {
         notification_arn = aws_sns_topic.example.arn
-        notification_events = ["Success", "Failed"]
+        notification_events = ["Success"]
         notification_type = "Command"
 
       }
@@ -186,3 +190,164 @@ resource "patch_group" "example" {
   patch_group = var.patch_group_tag_value  # Using maintenance window name as patch group name, adjust if needed
 }
 
+####################################################################
+####################################################################
+#lambda to start
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_execution_role"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "lambda.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name        = "lambda_policy"
+  description = "Policy for Lambda function"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : "logs:CreateLogGroup",
+        "Resource" : "arn:aws:logs:${var.region}:${var.account_id}:*"
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        "Resource" : [
+          "arn:aws:logs:${var.region}:${var.account_id}:log-group:/aws/lambda/${var.lambda_start_name}:*"
+        ]
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ec2:DescribeInstances",
+          "ec2:DescribeInstanceStatus",
+          "ec2:StartInstances",
+          "ec2:DescribeTags"
+        ],
+        "Resource" : "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  policy_arn = aws_iam_policy.lambda_policy.arn
+  role       = aws_iam_role.lambda_role.name
+}
+
+resource "aws_lambda_function" "lambda_start_function" {
+  function_name = var.lambda_start_name
+  architectures = ["x86_64"]
+  package_type  = "Zip"
+  filename      = "start_function_payload.zip"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.12"
+  timeout       = 15
+  memory_size   = 128
+  ephemeral_storage {
+    size = 512
+  }
+  logging_config {
+    log_format = "Text"
+    log_group  = "/aws/lambda/${var.lambda_start_name}/${var.mw_name}"
+  }
+}
+
+
+
+
+
+####################################################################
+####################################################################
+# Create Role for schedulers
+resource "aws_iam_role" "scheduler_role" {
+  name = "scheduler_execution_role"
+
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "scheduler.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole",
+        "Condition": {
+          "StringEquals": {
+            "aws:SourceAccount": [var.account_id]
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "scheduler_policy" {
+  name        = "scheduler_policy"
+  description = "Policy for AWS Scheduler execution"
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": "lambda:InvokeFunction",
+        "Resource": [
+          "aws_lambda_function.lambda_start_function.arn/*",
+          aws_lambda_function.lambda_start_function.arn
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "scheduler_policy_attachment" {
+  role       = aws_iam_role.scheduler_role.name
+  policy_arn = aws_iam_policy.scheduler_policy.arn
+}
+
+# Create aws_schedule_scheduler
+
+
+resource "aws_scheduler_schedule" "start" {
+  state = var.start_instance
+  name = "ollion-patch-instance-start-${var.mw_name}"
+  group_name = "default"
+
+  flexible_time_window {
+    mode = "FLEXIBLE"
+    maximum_window_in_minutes = 30
+  }
+
+  schedule_expression = "${terraform.output.adjusted_cron_expression}"
+  schedule_expression_timezone = var.schedule_timezone
+
+  target {
+    arn = "arn:aws:scheduler:::aws-sdk:lambda:invoke"
+    role_arn = aws_iam_role.scheduler_role.arn
+    input = {
+      "patchgroup_values": var.patch_group_tag_value
+    }
+  }
+  depends_on = [ aws_lambda_function.lambda_start_function, aws_iam_role.scheduler_role ]
+}
+
+####################################################################
+####################################################################
