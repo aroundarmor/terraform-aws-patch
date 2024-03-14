77ce6c108992ebaa8dcdd5894f04bbf2c2ca4f99
Sourav Patel <37928615+aroundarmor@users.noreply.github.com>
2024-03-14 16:58:35 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket         = "ollion-patching-terraform-state-in-${var.aws_region}-and-${var.aws_account_id}"
-    key            = "terraform.tfstate"
-    region         = var.aws_region
-    #dynamodb_table = "terraform-lock"
-    encrypt        = true
+  backend "local" {
+    path = "terraform.tfstate"
   }
 }
